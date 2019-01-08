84148b9a36e2c3a803cf989b2a7a6167d0783daa
yugeshreddypalvai <pyreddy517@hotmail.com>
2019-01-08 11:41:01 +0530

    Chap1: Ruby Data Types

---
+++ data_types.rb
@@ -0,0 +1,11 @@
+# Ruby Data Types
+
+puts "string".class
+puts 12.class
+puts 12.12.class
+puts true.class
+puts false.class
+puts ["obj1", 12, "obj2"].class
+puts ({first_name: 'yugesh', last_name: "palvai"}).class
+puts (1..5).class
+puts :some_symbol.class
