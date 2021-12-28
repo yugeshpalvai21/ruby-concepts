0dee49fb66147ea489c5abe1da406f84cb36cb8c
yugeshpalvai <yugeshpalvai21@gmail.com>
2021-12-28 16:40:35 +0530

    MetaProgramming - Class Duplication

---
+++ meta_programming/class_duplication.rb
@@ -0,0 +1,21 @@
+class Person
+end
+
+# p Person.new.first_name
+
+class Person
+  def first_name
+    "Yugesh"
+  end
+end
+
+# p Person.new.first_name
+
+class Person
+  def first_name
+    "Y U G E S H"
+  end
+end
+
+p Person.new.first_name
+
