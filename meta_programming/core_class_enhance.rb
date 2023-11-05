427fe30b591c783b30607a36e6ffec700a14a7d6
Yugesh Palvai <yugeshpalvai21@gmail.com>
2023-11-05 22:56:38 +0530

    Update core_class_enhance.rb

--- meta_programming/core_class_enhance.rb
+++ meta_programming/core_class_enhance.rb
@@ -1,7 +1,7 @@
 class Array
   # Adding new method to core functionality
   def multiply_all
-    self.reduce { |result, num| (result.to_i) * ( num.to_i == 0 ? 1 : num) } 
+    self.reduce(1) { |result, num| (result) * ( num.to_i == 0 ? 1 : num) } 
   end
 
   # Overriding inbuilt Array method
@@ -10,5 +10,5 @@ class Array
   end
 end
 
-p [1, 2, 4, 6, 'fdf', 'dfsdf', 2].multiply_all
+p ["gjhgjh", 2, 4, 6, 'fdf', 'dfsdf', 2].multiply_all
 p [1,2].sum
