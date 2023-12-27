df9a8862558607ff72bb45a588e83f04a6ee94e7
Yugesh Palvai <yugeshpalvai21@gmail.com>
2023-12-27 13:42:31 +0530

    explores rspec let and attr specifiers

--- 21_testing_with_ruby/lib/person.rb
+++ 21_testing_with_ruby/lib/person.rb
@@ -1,2 +1,11 @@
 class Person
+  attr_reader :age
+
+  def initialize(age: 0)
+    @age = age
+  end
+
+  def is_major?
+    age >= 18
+  end
 end
--- 21_testing_with_ruby/spec/person_spec.rb
+++ 21_testing_with_ruby/spec/person_spec.rb
@@ -1,8 +1,25 @@
 require "person"
 
 RSpec.describe Person do
-  it "instantiate person class" do
-    person = described_class.new
-    expect(person).to be_instance_of(Person)
+  let(:person_below_eighteen) { described_class.new(age: 16) }
+  let(:person_above_eighteen) { described_class.new(age: 19) }
+
+  it "instantiate person with default age and access specifier" do
+    expect(subject).to be_instance_of(Person)
+    expect(subject.age).to eql 0
+  end
+
+  it "instantiate person with given age and returns it" do
+    expect(person_below_eighteen.age).to eql(16)
+  end
+
+  describe "#is_major?" do
+    it "returns true when pesron is above 18 years old" do
+      expect(person_above_eighteen.is_major?).to be true
+    end
+
+    it "returns false when pesron is below 18 years old" do
+      expect(person_below_eighteen.is_major?).to be false
+    end
   end
 end
